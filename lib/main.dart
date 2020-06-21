2f85eadb23eb03ed919d53aaa3f3120272978d06
vaygeth89 <vaygeth@gmail.com>
2020-06-22 01:48:24 +0300

    added more comments

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
+      //By default makes your app always load the StartUpWidget
       home: StartUpWidget(),
     );
   }
